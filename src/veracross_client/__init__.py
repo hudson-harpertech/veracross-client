8c7711dd440ac250f22f1008759ab1ae86680bb6
hudson-harpertech <102432543+hudson-harpertech@users.noreply.github.com>
2023-09-11 21:53:53 -0700

    add documentation to __init__.py

--- src/veracross_client/__init__.py
+++ src/veracross_client/__init__.py
@@ -1,1 +1,10 @@
+__version__ = "0.0.4"
+"""Version of the veracross-client package."""
+
+""""
+Veracross Client
+----------------
+
+A Python client for the Veracross API.
+"""
 from .veracross_client import VeracrossClient
